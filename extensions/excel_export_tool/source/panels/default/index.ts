e4cf341d9433cc1a711df27ce911ccf4782c1df3
lxc <271039973@qq.com>
2026-07-08 15:18:42 +0800

    修改导表工具插件，将导出的json文件压缩成zip并保留成config.bin供游戏使用，游戏中只保留config.bin文件

--- extensions/excel_export_tool/source/panels/default/index.ts
+++ extensions/excel_export_tool/source/panels/default/index.ts
@@ -1,10 +1,11 @@
 /* eslint-disable vue/one-component-per-file */
 
-import { existsSync, readdirSync, readFileSync, writeFileSync } from 'fs-extra';
-import { join } from 'path';
+import { createWriteStream, existsSync, readdirSync, readFileSync, removeSync, statSync, writeFileSync } from 'fs-extra';
+import path, { join } from 'path';
 import { createApp, App, defineComponent } from 'vue';
 import CfgUtils from './CfgUtils';
 const panelDataMap = new WeakMap<any, App>();
+const archiver = require('archiver');
 /**
  * @zh 如果希望兼容 3.3 之前的版本可以使用下方的代码
  * @en You can add the code below if you want compatibility with versions prior to 3.3
@@ -17,10 +18,9 @@ interface IFileData {
     selected: boolean
 }
 
-interface IExportPathConfig {
-    exportPath: string,
-    exportTSPath: string,
-    excelPath: string
+interface IJsonData {
+    filePath: string,
+    isDelete: boolean
 }
 
 export const openDilog = async (type: string, title: string, message: string, btnMap?: Map<string, Function>, cancel?: number) => {
@@ -80,7 +80,8 @@ module.exports = Editor.Panel.define({
                             exportPath: '',
                             exportTSPath: '',
                             excelPath: ''
-                        }
+                        },
+                        jsonFiles: [] as IJsonData[]
                     };
                 },
                 methods: {
@@ -97,6 +98,12 @@ module.exports = Editor.Panel.define({
                                         selected: false
                                     });
                                 }
+                                if (item.endsWith('.json')) {
+                                    this.jsonFiles.push({
+                                        filePath: join(this.excelPath, item),
+                                        isDelete: false
+                                    });
+                                }
                             })
                         }
                         return files;
@@ -108,9 +115,70 @@ module.exports = Editor.Panel.define({
                     },
                     exportConfig() {
                         let exportFiles = this.files.filter((item) => item.selected).map((item) => join(this.excelPath, item.name));
-                        CfgUtils.parseExcel(exportFiles, this.exportPath, this.exportTSPath, this.isFormat, this.isArr, () => {
+                        CfgUtils.parseExcel(exportFiles, this.exportPath, this.exportTSPath, this.isFormat, this.isArr, async () => {
                             openDilog('info', 'info', '导表完成');
                             this.saveExportPathConfig();
+
+                            // 将json文件压缩成zip
+                            let zip = async function compressToZip(jsonDatas: IJsonData[], outputFileName: string): Promise<void> {
+                                return new Promise((resolve, reject) => {
+                                    const output = createWriteStream(outputFileName);
+
+                                    const archive = archiver('zip', { zlib: { level: 9 } });
+
+                                    output.on('close', () => {
+                                        console.log(`✅ 压缩完成: ${outputFileName} (${archive.pointer()} bytes)`);
+                                        resolve();
+                                    });
+
+                                    archive.on('error', (err) => {
+                                        reject(err);
+                                    });
+
+                                    archive.pipe(output);
+
+                                    for (const jData of jsonDatas) {
+                                        let { filePath } = jData;
+                                        if (!existsSync(filePath)) {
+                                            console.warn(`⚠️ 警告: 路径不存在，已跳过 -> ${filePath}`);
+                                            continue;
+                                        }
+
+                                        const stats = statSync(filePath);
+                                        if (stats.isDirectory()) {
+                                            archive.directory(filePath, path.basename(filePath), (entry: any) => {
+                                                return entry;
+                                            });
+                                        } else {
+                                            archive.file(filePath, { name: path.basename(filePath) });
+                                        }
+                                    }
+
+                                    archive.finalize();
+                                });
+                            }
+                            try {
+                                let jsonDatas: IJsonData[] = [];
+                                let dir = readdirSync(this.exportPath);
+                                dir.forEach((item) => {
+                                    if (item.endsWith('.json')) {
+                                        jsonDatas.push({
+                                            filePath: join(this.exportPath, item),
+                                            isDelete: true
+                                        });
+                                    }
+                                });
+                                jsonDatas = jsonDatas.concat(this.jsonFiles);
+
+                                await zip(jsonDatas, join(this.exportPath, 'config.bin'));
+                                jsonDatas.forEach((jData: IJsonData) => {
+                                    if (existsSync(jData.filePath) && jData.isDelete) {
+                                        removeSync(jData.filePath);
+                                    }
+                                });
+                            } catch (error) {
+                                openDilog('error', 'error', `zip失败,${error}`);
+                            }
                         });
                     },
                     changeExportPathConfig(isUse: boolean) {
